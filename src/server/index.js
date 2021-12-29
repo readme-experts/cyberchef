57f89ef95f757f46cb010bfed624fbda5d825dd5
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-30 01:49:55 +0200

    Basic server code

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Server index');
+const fastify = require('fastify')()
+const path = require('path');
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, '../client/html')
+});
+
+fastify.setNotFoundHandler((req, res) => {
+  res.sendFile('index.html');
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start().then();
