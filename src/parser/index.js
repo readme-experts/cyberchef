fd6c58c770b3df01abc00e2008db27e52bd30d1d
mrpaschenko <paschenkod2@gmail.com>
2022-01-02 20:55:20 +0200

    added puppeteer

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,5 +1,32 @@
 'use strict';
 
-const https = require('https');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+
+class PuppeteerHandler {
+  constructor() {
+    this.browser = null;
+  }
+  async initBrowser() {
+    this.browser = await puppeteer.launch(LAUNCH_PUPPETEER_OPTS);
+  }
+  closeBrowser() {
+    this.browser.close();
+  }
+  async getPageContent(url) {
+    if (!this.browser) {
+      await this.initBrowser();
+    }
+
+    try {
+      const page = await this.browser.newPage();
+      await page.goto(url, PAGE_PUPPETEER_OPTS);
+      const content = await page.content();
+      return content;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
 
