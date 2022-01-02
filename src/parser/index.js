f70daba7f41d4055ac97692b83e097e23eae1b96
mrpaschenko <paschenkod2@gmail.com>
2022-01-02 20:56:19 +0200

    added opts for puppeteer

--- src/parser/index.js
+++ src/parser/index.js
@@ -3,6 +3,23 @@
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 
+const LAUNCH_PUPPETEER_OPTS = {
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--disable-gpu',
+    '--window-size=1920x1080'
+  ]
+};
+
+const PAGE_PUPPETEER_OPTS = {
+  networkIdle2Timeout: 5000,
+  waitUntil: 'networkidle2',
+  timeout: 3000000
+};
+
 class PuppeteerHandler {
   constructor() {
     this.browser = null;
