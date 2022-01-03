0f5e2003123b967db1dc8a79985258f44250f904
yanulya <vladonyaroshchuk@gmail.com>
2022-01-03 22:53:03 +0200

    updated index base

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const cheerio = require('cheerio');
-const puppeteer = require('puppeteer');
+const chalk = require('chalk');
 
 const SITE = 'https://www.povarenok.ru/recipes/show/';
-const page = 100000;
-
+let page = 100000;
+(async function main() {
+  try {
+    for(;page < 100100; page++) {
+      const url = `${SITE}${page}`;
+      const pageContent = await getPageContent(url); // function is not ready yet
+    }
+  } catch {
+    console.log(chalk.red('An error has occured \n'));
+    console.log(err);
+  }
+})();
