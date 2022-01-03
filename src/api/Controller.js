7602ec3cac4c2cd35d27148d2fa69cb4ad71fb81
water-korra <uberigloo@gmail.com>
2022-01-03 22:20:31 +0200

    now recipes can be created and deleted by their id

--- src/api/Controller.js
+++ src/api/Controller.js
@@ -19,17 +19,16 @@ class Controller  {
         }
     }
 
-    // async getRecipe(req,res) {
-    //     try {
-    //         const recipeId = db.getRecipe()
-    //         const newRecipe = {name,categoryId,products,description,imageLink}
-    //         await db.addRecipeToDb(newRecipe.name,newRecipe.categoryId,newRecipe.products,
-    //             newRecipe.description, newRecipe.imageLink)
-    //     } catch (e) {
-    //         console.log(e)
-    //         res.status(400).json({message: 'Registration error'});
-    //     }
-    // }
+    async getRecipeById(req,res) {
+        try {
+            const {id} = req.body
+            const neededRecipe = await db.getRecipe(id)
+            return res.json({message: neededRecipe})
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({message: 'error by getting recipe by id'});
+        }
+    }
     async getAllRecipes(req,res) {
         const allRecipes = await db.getAllRecipes()
         return res.json({recipes: allRecipes})
@@ -38,12 +37,16 @@ class Controller  {
             res.status(400).json({message: 'Error ocured while getting recipes list'});
         }
     
-        async deleteRecipe(req,res) {
-            
-            } catch (e) {
-                console.log(e)
-                res.status(400).json({message: 'Registration error'});
-            }
+    async deleteRecipeById(req,res) {
+        try {
+            const {id} = req.body
+            await db.deleteRecipe(id)
+            return res.json({message: "recipe successfully deleted"})
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({message: 'error by deleting receipe'});
+        }
+    }
     
 
 }
--- src/api/router.js
+++ src/api/router.js
@@ -3,15 +3,15 @@ const router = new Router()
 const controller = require('./Controller')
 
 
-// router.post('recipes/:id' , Controller.handleCurrentRecipes) 
-// router.get('recipes/:id' , Controller.handleCurrentRecipes) 
+// router.post('/recipes/:id' , controller.handleCurrentRecipes) 
+// router.get('/recipes/:id' , controller.handleCurrentRecipes) 
 
 
 router.post('/user/recipes', controller.addRecipe)
- 
+router.get('/user/recipes', controller.getRecipeById)
+router.delete('/user/recipes', controller.deleteRecipeById)
 
 router.get('/user/myrecipes', controller.getAllRecipes) 
 
-// router.get('user/recipes', Controller.getOneRecipe) 
-// router.delete('user/recipes', controller.deleteOneRecipe)
+
 module.exports = router
