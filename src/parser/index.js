7d34717c00ce4a8361eca59dbb02c78caca50906
yanulya <vladonyaroshchuk@gmail.com>
2022-01-04 02:27:27 +0200

    got to a point where info links are parsed

--- src/parser/index.js
+++ src/parser/index.js
@@ -12,7 +12,18 @@ const pages = 2;
     for (const page of common.arrayFromLength(pages)) {
       const url = `${SITE}~${page}`;
       const pageContent = await pupp.getPageContent(url);
-      console.log(pageContent);
+      //console.log(pageContent);
+      const $ = cheerio.load(pageContent);
+      const items = [];
+
+      $('.desktop-img').each((i, header) => {
+        const url = $(header).find('a').attr('href');
+
+        items.push({
+          url,
+        });
+      });
+      console.log(items);
     }
   } catch (err) {
     console.log(chalk.red('An error has occured \n'));
