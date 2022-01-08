5bdbf8abc3f1648bfe428a0c3257c59afd02addf
yanulya <vladonyaroshchuk@gmail.com>
2022-01-08 16:56:03 +0200

    added error handler for saver

--- src/parser/handlers/saver.js
+++ src/parser/handlers/saver.js
@@ -3,13 +3,17 @@
 const fs = require('fs');
 
 async function saveData(data) {
-  const json = JSON.stringify(data);
-  fs.writeFile('./data/recipes.json', json, err => {
-    if (err) {
-      return console.log(err);
-    }
-    //console.log('The file was saved!');
-  });
+  try {
+    const json = JSON.stringify(data);
+    fs.writeFile('./data/recipes.json', json, err => {
+      if (err) {
+        return console.log(err);
+      }
+      //console.log('The file was saved!');
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = {
