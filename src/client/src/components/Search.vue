8a99800dd6a340b5f9d0669d8fddfd7f5c54383e
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 18:18:29 +0200

    Search component logic

--- src/client/src/components/Search.vue
+++ src/client/src/components/Search.vue
@@ -1,9 +1,15 @@
 <template>
-  <form class="content__filter">
+  <form class="content__filter" @submit.prevent="$emit('submitQuery', query)">
     <h3>Find recipes</h3>
     <label for="search" class="content__label"></label>
     <div>
-      <input type="text" name="search" id="search" class="content__search" />
+      <input
+        type="text"
+        name="search"
+        id="search"
+        class="content__search"
+        v-model="query"
+      />
       <button type="submit" class="content__submit">Search</button>
     </div>
   </form>
@@ -12,6 +18,11 @@
 <script>
 export default {
   name: 'Search',
+  data() {
+    return {
+      query: '',
+    };
+  },
 };
 </script>
 
