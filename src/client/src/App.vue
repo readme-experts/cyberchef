a71b1592bcd57655a49e8fc83a9d8edfe18cdf03
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 17:20:22 +0200

    Auth intercept

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -3,3 +3,18 @@
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  name: 'App',
+  mounted: function () {
+    this.$http.interceptors.response.use(null, (err) => {
+      return new Promise(() => {
+        if (err.status === 403) {
+          this.$store.dispatch('logout');
+        }
+        throw err;
+      });
+    });
+  },
+};
+</script>
