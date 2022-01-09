a5bcc21e4a59806cdd80777f33d374013598e70a
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 17:18:48 +0200

    Receipt view

---
+++ src/client/src/views/Receipt.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header></Header>
+    <Receipt></Receipt>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Receipt from '@/components/Receipt';
+import Footer from '@/components/Footer';
+export default {
+  name: 'Receipt',
+  components: { Header, Receipt, Footer },
+};
+</script>
+
+<style scoped></style>
