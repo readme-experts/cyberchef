44521e988d3ef9667678d641d27dfd8af3c9c7a6
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 03:16:19 +0200

    AllRecipes component

---
+++ src/client/src/components/RecipesSearch.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="container">
+    <div class="content">
+      <form class="content__filter">
+        <h3>Find recipes</h3>
+        <label for="search" class="content__label"></label>
+        <div>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            class="content__search"
+          />
+          <button type="submit" class="content__submit">Search</button>
+        </div>
+      </form>
+      <div v-if="recipes" class="content__recipes">
+        <Recipe v-for="recipe of recipes" :key="recipe.id"></Recipe>
+      </div>
+      <h3 v-else>No recipes</h3>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'RecipesSearch',
+};
+</script>
+
+<style scoped></style>
