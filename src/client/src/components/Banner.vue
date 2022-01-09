3da181c3c2bb7503a2006107670c1f7e249ed260
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 03:12:13 +0200

    Banner component

---
+++ src/client/src/components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="container">
+    <div class="banner">
+      <h3>Cyberchef - light and easy to use cooking ideas app</h3>
+      <p>Start now</p>
+      <div class="banner__buttons">
+        <router-link to="/login" class="banner__href">Login</router-link>
+        <router-link to="/register" class="banner__href">Register</router-link>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+};
+</script>
+
+<style scoped></style>
