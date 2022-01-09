0ff6a72e7c9d0bc88356198568c071c427372b0d
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 03:33:27 +0200

    Initial login component

---
+++ src/client/src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <form action="#" class="login">
+      <h3 class="login__heading">Login</h3>
+      <div>
+        <label for="name" class="login__label">Email</label><br />
+        <input
+          type="text"
+          name=""
+          id="name"
+          class="login__input"
+          placeholder="example@email.com"
+          required
+        />
+      </div>
+      <div>
+        <label for="pass" class="login__label">Password</label><br />
+        <input type="text" name="" id="pass" class="login__input" required />
+      </div>
+      <a href="#" class="login__register">Don`t have an account?</a>
+      <button type="submit" class="login__submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style scoped></style>
