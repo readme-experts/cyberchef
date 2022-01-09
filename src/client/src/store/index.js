04af7410eacd8266848d8a4843441c6224d7e6a6
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 04:22:20 +0200

    Vuex store logic

--- src/client/src/store/index.js
+++ src/client/src/store/index.js
@@ -1,11 +1,82 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    status: '',
+    token: localStorage.getItem('token') || '',
+    user: {},
+    receipts: [],
+  },
+  mutations: {
+    authRequest(state) {
+      state.status = 'loading';
+    },
+    authSuccess(state, payload) {
+      state.status = 'success';
+      state.token = payload.token;
+      state.user = JSON.parse(localStorage.getItem('user')) || payload.user;
+    },
+    authError(state) {
+      state.status = 'error';
+    },
+    logout(state) {
+      state.status = '';
+      state.token = '';
+      state.user = '{}';
+    },
+    loadReceipts(state, payload) {
+      state.receipts = [...payload.receipts];
+    },
+  },
+  actions: {
+    async login({ commit }, user) {
+      commit('auth_request');
+      try {
+        const r = await axios({
+          url: '/auth/login',
+          data: user,
+          method: 'POST',
+        });
+        const token = r.data.token;
+        const responseUser = r.data.user;
+        localStorage.setItem('token', token);
+        localStorage.setItem('user', JSON.stringify(responseUser));
+        axios.defaults.headers.common.usertoken = token;
+        commit('auth_success', { token, responseUser });
+        return r;
+      } catch (err) {
+        commit('auth_error');
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        throw err;
+      }
+    },
+    async register({ commit }, user) {
+      commit('auth_request');
+      return await axios({
+        url: '/auth/registration',
+        data: user,
+        method: 'POST',
+      }).catch(() => alert('Something went wrong, please try again'));
+    },
+    async logout({ commit }) {
+      commit('logout');
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      return delete axios.defaults.headers.authorization;
+    },
+    async receipts({ commit }, receipts) {
+      commit('loadReceipts', receipts);
+    },
+  },
+  getters: {
+    isLoggedIn: (state) => !!state.token,
+    authStatus: (state) => state.status,
+    user: (state) => state.user,
+    receipts: (state) => state.receipts,
+  },
 });
