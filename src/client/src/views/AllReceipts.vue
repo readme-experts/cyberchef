07b5bba8d9c376ac48b4cd50f9bbd65a0c150ba8
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 03:56:18 +0200

    AllReceipts.vue view

--- src/client/src/views/AllReceipts.vue
+++ src/client/src/views/AllReceipts.vue
@@ -1,13 +1,23 @@
 <template>
-
+  <div>
+    <Header></Header>
+    <ReceiptsSearch></ReceiptsSearch>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
+import ReceiptsSearch from '@/components/ReceiptsSearch';
+import Footer from '@/components/Footer';
 export default {
   name: 'AllReceipts',
+  components: {
+    Header,
+    ReceiptsSearch,
+    Footer,
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
