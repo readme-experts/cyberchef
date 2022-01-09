69d87b01be4d12c6a37421135a719df8e0aaec07
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 03:05:41 +0200

    Header component

---
+++ src/client/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <div class="header__left">
+      <a href="#" class="header__logo_link">
+        <h3 class="header__logo"><span>Cyber</span>chef</h3>
+      </a>
+    </div>
+    <div class="header__right">
+      <p class="header__username">{{ user.name }}</p>
+      <img :src="user.avatar" alt="avatar" class="header__avatar" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    user: Object,
+  },
+};
+</script>
+
+<style scoped></style>
