a1c853ba8a1cdcbbab37a44e3e3d4e2bace9b25c
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 03:34:01 +0200

    Initial register component

---
+++ src/client/src/views/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <form action="#" class="login">
+      <h3 class="login__heading">Register</h3>
+      <div>
+        <label for="name" class="login__label">Email</label><br />
+        <input
+          type="text"
+          name=""
+          id="name"
+          class="login__input"
+          placeholder="example@email.com"
+          required
+        />
+      </div>
+      <div>
+        <label for="pass" class="login__label">Password</label><br />
+        <input type="text" name="" id="pass" class="login__input" required />
+      </div>
+      <div>
+        <label for="pass" class="login__label">Confirm password</label><br />
+        <input type="text" name="" id="confirm" class="login__input" required />
+      </div>
+      <a href="#" class="login__register">Already have an account?</a>
+      <button type="submit" class="login__submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+};
+</script>
+
+<style scoped></style>
