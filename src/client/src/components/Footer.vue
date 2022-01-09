abb4ff7b7892ee1393949ce35ed7804280f50dda
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 03:09:37 +0200

    Footer component

---
+++ src/client/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <div class="footer__container">
+      <div class="footer__left">
+        <p class="footer__desc">
+          Copyright ©Readme Experts 2021 All rights reserved
+        </p>
+      </div>
+      <div class="footer__right">
+        <a href="/.github" class="footer__link">Github</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped></style>
