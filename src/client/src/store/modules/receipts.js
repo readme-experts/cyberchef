2048d7c292d558478aac47b09cc609c2ff839be7
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 17:37:40 +0200

    receipts module

---
+++ src/client/src/store/modules/receipts.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default {
+  state: {
+    receipts: [],
+  },
+  getters: {
+    receipts: (state) => [...state.receipts],
+    recieptById: (state) => (id) =>
+      state.receipts.find((receipt) => receipt.id === id),
+  },
+  mutations: {
+    updateReceipts(state, payload) {
+      state.receipts = [...payload.data];
+    },
+  },
+  actions: {
+    async loadReceipts({ commit }, recipeName) {
+      const result = await axios
+        .post('/receipts', { recipeName })
+        .catch((e) => console.log(e));
+      commit('updateReceipts', [...result.data]);
+    },
+  },
+};
