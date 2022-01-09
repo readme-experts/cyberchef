dd0592d46293216c9c40ed89580ceeb7bbcf9585
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 17:08:58 +0200

    OneReceipt component

---
+++ src/client/src/components/OneReceipt.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="container">
+    <img :src="receipt.img" alt="Receipt image" />
+    <h3>{{ receipt.title }}</h3>
+    <p>receipt.text</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'OneReceipt',
+  data() {
+    return {
+      receipt: this.$store.getters.receipt,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
