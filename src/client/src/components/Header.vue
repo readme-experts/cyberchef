b7324801a92191aff1241b0dfe29e4a3c31a6929
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 03:39:03 +0200

    Fix header links

--- src/client/src/components/Header.vue
+++ src/client/src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
   <header class="header">
     <div class="header__left">
-      <router-link :to="{ path: `/user/${user.id}` }" class="header__logo_link">
+      <router-link to="/home" class="header__logo_link">
         <h3 class="header__logo"><span>Cyber</span>chef</h3>
       </router-link>
     </div>
     <div class="header__right">
-      <p class="header__username">{{ user.name }}</p>
-      <img :src="user.avatar" alt="avatar" class="header__avatar" />
+      <router-link :to="{ path: `/user/${user.id}` }" class="header__logo_link">
+        <p class="header__username">{{ user.name }}</p>
+        <img :src="user.avatar" alt="avatar" class="header__avatar" />
+      </router-link>
     </div>
   </header>
 </template>
