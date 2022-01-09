97ae5dc39b99a22dac3819d22c80e8599db3844b
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 17:21:47 +0200

    Auth check on mount

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -6,10 +6,18 @@ import store from './store';
 import VueHTTP from './plugins/http';
 
 Vue.config.productionTip = false;
-Vue.use(VueHTTP);
 
-new Vue({
+const app = new Vue({
   router,
   store,
+  VueHTTP,
   render: (h) => h(App),
 }).$mount('#app');
+
+const token = localStorage.getItem('token');
+const user = localStorage.getItem('user');
+if (token && user) {
+  app.$http.defaults.headers.common.authorization = token;
+  const res = JSON.parse(user);
+  app.$store.commit('auth_success', { token, res });
+}
