0091f608dd6dbf53f95f0784df2031b696070e9d
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 04:04:48 +0200

    Basic router config

--- src/client/src/router/index.js
+++ src/client/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
+import Login from '@/views/Login';
+import UserPage from '@/views/UserPage';
+import AllReceipts from '@/views/AllReceipts';
 
 Vue.use(VueRouter);
 
@@ -11,13 +14,30 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Login,
+  },
+  {
+    path: '/user/:id',
+    name: 'User',
+    component: UserPage,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/receipts',
+    name: 'AllReceipts',
+    component: AllReceipts,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
