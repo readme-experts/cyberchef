13fad4a6715176e23626648932bf7dc1f51a4f9e
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2022-01-09 04:00:11 +0200

    User component

---
+++ src/client/src/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="container">
+    <div class="content">
+      <h3>Here is your precious recipes you added to favourites</h3>
+      <div v-if="recipes" class="content__recipes">
+        <Recipe v-for="recipe of recipes" :key="recipe.id"></Recipe>
+      </div>
+      <h3 v-else>No recipes</h3>
+    </div>
+  </main>
+</template>
+
+<script>
+import Recipe from '@/components/Recipe';
+
+export default {
+  name: 'User',
+  components: {
+    Recipe,
+  },
+  data() {
+    return {
+      recipes: null,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
