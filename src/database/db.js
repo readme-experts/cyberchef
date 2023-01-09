65b425d0b59ec182b58b945aef5547beb774f0c8
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-01-09 12:45:07 +0200

    add getUserFavRecipes func function to get an array of user favourite recipes ids

--- src/database/db.js
+++ src/database/db.js
@@ -10,9 +10,26 @@ async function getAllRecipes() {
   return allRecipes; //returns an array of all recipes
 }
 
+async function getUserFavRecipes(userId) {
+  let favRecipes = await prisma.favourite_recipes.findMany({
+    where: {
+      user_id: userId,
+    },
+    select: {
+      recipe_id: true,
+    },
+  });
+  favRecipes = favRecipes.map(el => el.recipe_id);
+  return favRecipes; //returns an array user favourite recipes ids
+}
+
 (async () => {
   await prisma.$connect();
-  const recipes = await getAllRecipes();
+  const recipes = await getUserFavRecipes(3);
   console.log(recipes);
 })();
 
+module.exports = {
+  getAllRecipes,
+  getUserFavRecipes,
+};
