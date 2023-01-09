147ceff518c4e7a862c55594201394e920e50304
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-01-09 12:54:22 +0200

    add getRecipe func gets recipe from database by id

--- src/database/db.js
+++ src/database/db.js
@@ -5,6 +5,15 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+async function getRecipe(recipeId) {
+  const recipe = await prisma.recipes.findUnique({
+    where: {
+      id: recipeId,
+    },
+  });
+  return recipe; //return recipe object
+}
+
 async function getAllRecipes() {
   const allRecipes = await prisma.recipes.findMany();
   return allRecipes; //returns an array of all recipes
@@ -19,17 +28,18 @@ async function getUserFavRecipes(userId) {
       recipe_id: true,
     },
   });
-  favRecipes = favRecipes.map(el => el.recipe_id);
+  favRecipes = favRecipes.map((el) => el.recipe_id);
   return favRecipes; //returns an array user favourite recipes ids
 }
 
 (async () => {
   await prisma.$connect();
-  const recipes = await getUserFavRecipes(3);
+  const recipes = await getRecipe(3);
   console.log(recipes);
 })();
 
 module.exports = {
   getAllRecipes,
   getUserFavRecipes,
+  getRecipe,
 };
