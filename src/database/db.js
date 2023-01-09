93a83bca7123bce27bf78fc7c2dff74634154024
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-01-09 12:31:00 +0200

    add getAllRecipes function gets all recipes from database

--- src/database/db.js
+++ src/database/db.js
@@ -1,27 +1,18 @@
 'use strict';
 
-require('dotenv').config()
+require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const user = await prisma.users.create({
-    data: {
-      username: 'Alice2',
-      email: 'alice2@prisma.io',
-      password: 'sdfssfd'
-    },
-  });
-  console.log(user);
+async function getAllRecipes() {
+  const allRecipes = await prisma.recipes.findMany();
+  return allRecipes; //returns an array of all recipes
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+(async () => {
+  await prisma.$connect();
+  const recipes = await getAllRecipes();
+  console.log(recipes);
+})();
+
