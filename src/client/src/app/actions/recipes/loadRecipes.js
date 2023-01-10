269bc109d3ceb3f43ef096f6b8d85ba933f61461
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-10 18:32:52 +0200

    loadRecipes thunk

---
+++ src/client/src/app/actions/recipes/loadRecipes.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const addUserRecipe = createAsyncThunk(
+  'recipes/loadRecipes',
+  async ({ queryString, token }, { rejectWithValue }) => {
+    try {
+      const headers = {
+        'Content-Type': 'application/json',
+        'authorization': token
+      };
+      const params = new URLSearchParams({ recipeName: queryString });
+      const response = await fetch('/api/recipes?' + params, {
+        method: 'GET',
+        headers,
+      });
+      if (!response.ok) {
+        return rejectWithValue(await response.json());
+      }
+      return {
+        ...await response.json(),
+      };
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+
+  },
+);
