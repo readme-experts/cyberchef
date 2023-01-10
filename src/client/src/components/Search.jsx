18ab53984c7de9817cd17f492b1129ddc306370a
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-10 21:29:48 +0200

    Search component

---
+++ src/client/src/components/Search.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Search.propTypes = {
+  searchCallback: PropTypes.func
+};
+
+function Search({ searchCallback }) {
+  let formData = {
+    queryString: ''
+  };
+  const handleChange = e => {
+    formData = {
+      ...formData,
+      [e.target.name]: e.target.value.trim(),
+    };
+  };
+
+  return (
+    <form className="content__filter" onSubmit={event => searchCallback(event, formData)}>
+      <h3>Find recipes</h3>
+      <label htmlFor="search" className="content__label"></label>
+      <div>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="content__search"
+          onChange={event => handleChange(event)}
+        />
+        <button type="submit" className="content__submit">Search</button>
+      </div>
+    </form>
+  );
+}
+
+export default Search;
