7b6dfe17455c936985cbc27df790fd02f8f77fe8
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-10 21:28:20 +0200

    Added proptypes to protected route

--- src/client/src/features/ProtectedRoute.jsx
+++ src/client/src/features/ProtectedRoute.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const ProtectedRoute = ({ user, children }) => {
   if (!user) {
@@ -8,6 +9,12 @@ const ProtectedRoute = ({ user, children }) => {
   return children;
 };
 
-
+ProtectedRoute.propTypes = {
+  user: PropTypes.shape({
+    userRecipes: PropTypes.array.isRequired,
+    username: PropTypes.string,
+  }).isRequired,
+  children: PropTypes.func.isRequired
+};
 
 export default ProtectedRoute;
