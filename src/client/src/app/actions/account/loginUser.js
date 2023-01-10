81bb5f1ad4b1ecd02ab2ce36ff9271c49dea4ec7
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-10 18:31:41 +0200

    loginUser thunk

---
+++ src/client/src/app/actions/account/loginUser.js
@@ -0,0 +1,33 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const loginUser = createAsyncThunk(
+  'account/login',
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const headers = {
+        'Content-Type': 'application/json',
+      };
+      const data = {
+        email, password,
+      };
+      const response = await fetch('auth/login', {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        return rejectWithValue(await response.json());
+      }
+      return {
+        ...await response.json(),
+      };
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+
+  },
+);
