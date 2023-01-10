f12acfe83b7d95345e9d94bcdb6a4018ad90402e
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-10 02:15:04 +0200

    Initial router config

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AppRoutes, ProtectedAppRoutes } from './features/AppRoutes';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProtectedRoute from './features/ProtectedRoute';
 
 function App() {
+  const [user] = useState(null);
+
   return (
     <div className="App">
-
+      <Header />
+      <Routes>
+        {AppRoutes.map((route, index) => {
+          const { element, ...rest } = route;
+          return <Route key={index} {...rest} element={element}/>;
+        })}
+        {ProtectedAppRoutes.map((route, index) => {
+          const { element, ...rest } = route;
+          return <Route key={index} {...rest} element={
+            <ProtectedRoute user={user}>
+              {element}
+            </ProtectedRoute>}/>;
+        })}
+        <Route path="*" element={<p>There&amp;s nothing here: 404!</p>} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
