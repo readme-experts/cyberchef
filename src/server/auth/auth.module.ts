edf2fa2782b4ed67f6404cffdd3e628db52c4a34
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-11 18:09:57 +0200

    Nest cli auth example

--- src/server/auth/auth.module.ts
+++ src/server/auth/auth.module.ts
@@ -1,8 +1,7 @@
-import { PrismaService } from './../prisma.service';
 import { jwtConstants } from './constants';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
-import { UserModule } from './../user/user.module';
+import { UserModule } from '../user/user.module';
 import { Module } from "@nestjs/common";
 import {AuthService} from './auth.service'
 import { PassportModule } from '@nestjs/passport';
@@ -13,7 +12,7 @@ import { JwtModule } from '@nestjs/jwt';
         secret : 'SECRET',
         signOptions : {expiresIn: '60s'},
     })],
-    providers: [AuthService, LocalStrategy, JwtStrategy], 
+    providers: [AuthService, LocalStrategy, JwtStrategy],
     exports : [AuthService]
 })
 export class AuthModule{}
--- src/server/auth/auth.service.ts
+++ src/server/auth/auth.service.ts
@@ -1,25 +1,28 @@
-import { UserService } from './../user/user.service';
+import { UserService } from '../user/user.service';
 import { Injectable } from "@nestjs/common";
 import { JwtService } from '@nestjs/jwt';
 
 
-@Injectable() 
+@Injectable()
 export class AuthService{
     constructor(private userService: UserService, private jwtService : JwtService) {}
 
     async validateUser(username : string, password: string): Promise<any> {
         const user = await this.userService.findUser(username)
         console.log(user);
-        
+
 
         if(user && user.password === password ) {
             return user
         }
-        return null 
+        return null
     }
 
    async login(user: any) {
-       const payload  = { username : user.username, id : user.id}
+       const payload  = { username : user.username, id : user.id};
+
+       console.log(payload, this.jwtService)
+
        return {
            access_token : this.jwtService.sign(payload)
        }
