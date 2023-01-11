1581201b358e25a27f8b12599614a99c1af0b94d
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-11 19:11:17 +0200

    ValidationError class for validating forms

---
+++ src/client/src/features/ValidationError.js
@@ -0,0 +1,24 @@
+export default class ValidationError {
+  constructor(type, description) {
+    this.type = type;
+    this.description = description;
+  }
+
+  static Validate(data) {
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const validationErrors = [];
+    if (!emailRegex.test(data.email)) validationErrors.push(new ValidationError(
+      'email', 'Invalid email'
+    ));
+    if (data.password.length < 8) validationErrors.push(new ValidationError(
+      'password', 'Password is shorter that 8 symbols'
+    ));
+    if (data.confirm && data.confirm !== data.password) validationErrors.push(new ValidationError(
+      'password', 'Password does not match'
+    ));
+    return validationErrors;
+  }
+  toString() {
+    return this.description;
+  }
+}
