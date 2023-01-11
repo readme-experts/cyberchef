fa13d9ec8e45b12cb3821c823f98b7777d345889
water-korra <uberigloo@gmail.com>
2023-01-11 18:44:04 +0200

    auth controller return jwt token

--- src/server/auth/auth.service.ts
+++ src/server/auth/auth.service.ts
@@ -1,36 +1,41 @@
-import { UserService } from '../user/user.service';
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
-
 @Injectable()
-export class AuthService{
-    constructor(private userService: UserService, private jwtService : JwtService) {}
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
 
-    async validateUser(username : string, password: string): Promise<any> {
-        const user = await this.userService.findUser(username)
-        console.log(user);
+  // async validateUser(username: string, password: string): Promise<any> {
+  //   // //const user = await this.userService.findUser(username);
+  //   // console.log(user);
+  //   //
+  //   // if (user && user.password === password) {
+  //   //   return user;
+  //   // }
+  //   // return null;
+  // }
 
+  async login(user: any) {
+    const payload = { username: user.username, id: user.id };
 
-        if(user && user.password === password ) {
-            return user
-        }
-        return null
-    }
-
-   async login(user: any) {
-       const payload  = { username : user.username, id : user.id};
-
-       console.log(payload, this.jwtService)
-
-       return {
-           access_token : this.jwtService.sign(payload)
-       }
-   }
+    console.log(payload, this.jwtService);
 
     return {
       access_token: this.jwtService.sign(payload),
     };
   }
 
+  async register(user: any) {
+    const payload = { username: user.username, id: user.id };
+
+    console.log(payload, this.jwtService);
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async hello() {
+    return 'hello';
+  }
 }
