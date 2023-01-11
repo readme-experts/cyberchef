24fae79b6a97f307b1e65db6ca1ffca07950f649
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-11 21:47:36 +0200

    Added useNavigate

--- src/client/src/pages/Register.jsx
+++ src/client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import ValidationError from '../features/ValidationError';
 import { registerUser } from '../app/actions/account/registerUser';
@@ -8,6 +8,7 @@ import { registerUser } from '../app/actions/account/registerUser';
 function Register() {
   const [errors, setErrors] = useState([]);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   let formData = {
     email: '',
     password: '',
@@ -32,7 +33,7 @@ function Register() {
       email: formData.email,
       password: formData.password,
     }));
-    return <Navigate to='/recipes' />;
+    return navigate('/recipes');
   };
   const failedValidationStyles = {
     'outline': '1px solid red',
@@ -47,38 +48,45 @@ function Register() {
           <label htmlFor='name' className='login__label'>Email</label><br />
           <input
             type='text'
-            name=''
-            id='name'
+            name='email'
+            id='email'
             className='login__input'
             placeholder='example@email.com'
             required
             onChange={handleChange}
             style={errors.some(error => error.type === 'email') ? failedValidationStyles : {}}
           />
+          {errors.some(error => error.type === 'email') ? <p style={{ 'color': 'red' }}>Invalid email</p> : <></>}
         </div>
         <div>
           <label htmlFor='pass' className='login__label'>Password</label><br />
           <input
             type='password'
-            name=''
-            id='pass'
+            name='password'
+            id='password'
             className='login__input'
             required
             onChange={handleChange}
             style={errors.some(error => error.type === 'password') ? failedValidationStyles : {}}
           />
+          {errors.some(error => error.type === 'password') ? <p style={{ 'color': 'red' }}>
+            Password is shorter than 8 symbols
+          </p> : <></>}
         </div>
         <div>
           <label htmlFor='pass' className='login__label'>Confirm password</label><br />
           <input
             type='password'
-            name=''
+            name='confirm'
             id='confirm'
             className='login__input'
             required
             onChange={handleChange}
-            style={errors.some(error => error.type === 'password') ? failedValidationStyles : {}}
+            style={errors.some(error => error.type === 'confirm') ? failedValidationStyles : {}}
           />
+          {errors.some(error => error.type === 'password') ? <p style={{ 'color': 'red' }}>
+            Password does not match
+          </p> : <></>}
         </div>
         <Link to='/login' className='login__register'
         >Already have an account?</Link
