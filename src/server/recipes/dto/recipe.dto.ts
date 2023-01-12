a475e5ac24a9dddabd3b47790527418992c74b09
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-12 22:19:52 +0200

    Fixed names

--- src/server/recipes/dto/recipe.dto.ts
+++ src/server/recipes/dto/recipe.dto.ts
@@ -1,7 +1,7 @@
 export class CreateRecipeDto {
   readonly name: string;
-  readonly category_id: number;
+  readonly categoryId: number;
   readonly products: string;
   readonly description: string;
-  readonly image_link?: string;
+  readonly imageLink?: string;
 }
