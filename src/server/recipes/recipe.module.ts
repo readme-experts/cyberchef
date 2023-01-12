16cd2d6d6f34737abd8d68e4df31d7e10cef07e5
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-12 22:54:09 +0200

    Move prismaService to prisma folder

--- src/server/recipes/recipe.module.ts
+++ src/server/recipes/recipe.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
+import { RecipeRepository } from '../repositories/recipe.repository';
+import { CategoryRepository } from '../repositories/category.repository';
 
 @Module({
   controllers: [RecipeController],
-  providers: [RecipeService],
+  providers: [RecipeService, RecipeRepository, CategoryRepository],
 })
 export class RecipeModule {}
--- src/server/recipes/recipe.service.ts
+++ src/server/recipes/recipe.service.ts
@@ -1,9 +1,9 @@
-import CategoryRepository from '../../database/repositories/category';
-import RecipeRepository from '../../database/repositories/recipe';
 import { Injectable } from '@nestjs/common';
 import { CreateRecipeDto } from './dto/recipe.dto';
 import { recipeCategories } from './recipeCategories';
 import { RecipeEntity } from '../prisma/Entities/recipe.entity';
+import { RecipeRepository } from '../repositories/recipe.repository';
+import { CategoryRepository } from '../repositories/category.repository';
 
 @Injectable()
 export class RecipeService {
@@ -23,7 +23,7 @@ export class RecipeService {
   async getAllRecipes(): Promise<RecipeEntity[]> {
     return await this.recipe.findAll();
   }
-  async getRecipeByName(recipeName): Promise<RecipeEntity> {
+  async getRecipeByName(recipeName): Promise<RecipeEntity[]> {
     return await this.recipe.search(recipeName);
   }
 
