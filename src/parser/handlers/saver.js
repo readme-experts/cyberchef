e2cd25f3b8fa6af5b7489d10feb48b80a92b0d6b
thank1ess <vladonyaroshchuk@gmail.com>
2023-01-13 00:41:36 +0200

    modified saver.js

--- src/parser/handlers/saver.js
+++ src/parser/handlers/saver.js
@@ -6,9 +6,8 @@ async function saveData(data) {
   try {
     const json = JSON.stringify(data);
     fs.writeFile('./data/recipes.json', json, err => {
-      if (err) {
-        return console.log(err);
-      }
+      if (err) throw err;
+      console.log('The file has been saved.');
     });
   } catch (err) {
     console.log(err);
