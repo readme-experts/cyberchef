3a5cf6d0fadc56386ecd5a2af7b04961ff677314
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-01-12 22:20:25 +0200

    Refactored repos to nest style

---
+++ src/server/repos/recipe.repository.ts
@@ -0,0 +1,71 @@
+import { PrismaService } from '../prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateRecipeDto } from '../recipes/DTO/recipe.dto';
+import { RecipeEntity } from '../recipes/Entities/recipe.entity';
+
+@Injectable()
+export class RecipeRepository {
+  constructor(private prisma: PrismaService) {
+    this.prisma = prisma;
+  }
+
+  async add(recipeData: CreateRecipeDto) {
+    try {
+      await this.prisma.recipes.create({
+        data: {
+          name: recipeData.name,
+          category_id: +recipeData.categoryId,
+          products: recipeData.products,
+          description: recipeData.description,
+          image_link: recipeData.imageLink,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  async find(recipeId: string): Promise<RecipeEntity> {
+    return this.prisma.recipes.findUnique({
+      where: {
+        id: parseInt(recipeId),
+      },
+    });
+  }
+
+  async findAll(): Promise<RecipeEntity[]> {
+    return this.prisma.recipes.findMany();
+  }
+
+  async delete(recipeId: string): Promise<boolean> {
+    try {
+      const deleteFavRecipes = this.prisma.favourite_recipes.deleteMany({
+        where: {
+          recipe_id: parseInt(recipeId),
+        },
+      });
+      const deleteRecipe = this.prisma.recipes.delete({
+        where: {
+          id: parseInt(recipeId),
+        },
+      });
+      await this.prisma.$transaction([deleteFavRecipes, deleteRecipe]);
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  async search(str: string): Promise<RecipeEntity[]> {
+    return this.prisma.recipes.findMany({
+      where: {
+        name: {
+          contains: str,
+        },
+      },
+    });
+  }
+}
---
+++ src/server/repos/user.repository.ts
@@ -0,0 +1,70 @@
+import { PrismaService } from '../prisma.service';
+import { RegisterUserDto } from '../user/dto/registration.dto';
+import { Injectable } from '@nestjs/common';
+import { UserEntity } from '../user/Entities/user.entity';
+
+@Injectable()
+export class UserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async add(userData: RegisterUserDto): Promise<boolean> {
+    try {
+      await this.prisma.users.create({
+        data: {
+          email: userData.email,
+          username: userData.username,
+          password: userData.passwordHash,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  async find(username: string): Promise<UserEntity> {
+    return this.prisma.users.findUnique({
+      where: {
+        username,
+      },
+    }); //returns user object
+  }
+
+  async findRecipes(userId: string) {
+    const favRecipes = await this.prisma.favourite_recipes.findMany({
+      where: {
+        user_id: parseInt(userId),
+      },
+      select: {
+        recipe_id: true,
+      },
+    });
+    return favRecipes.map(el => el.recipe_id); //returns an array user favourite recipes ids
+  }
+
+  async addRecipe(recipeData): Promise<boolean> {
+    const favRecipe = await this.prisma.favourite_recipes.create({
+      data: {
+        user_id: recipeData.userId,
+        recipe_id: recipeData.recipeId,
+      },
+    });
+    return !!(typeof favRecipe !== 'undefined' && favRecipe);
+  }
+
+  async deleteRecipe(recipeData): Promise<boolean> {
+    try {
+      await this.prisma.favourite_recipes.deleteMany({
+        where: {
+          user_id: recipeData.userId,
+          recipe_id: recipeData.recipeId,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+}
