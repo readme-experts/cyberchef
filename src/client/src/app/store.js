a433a996c75dab4dcb30dfe9d842fe7b193b39ba
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-05-31 19:20:27 +0300

    feat: added services to store creation

--- src/client/src/app/store.js
+++ src/client/src/app/store.js
@@ -1,6 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import accountReducer from './slices/accountSlice';
 import recipesReducer from './slices/recipesSlice';
+import AuthService from '../services/AuthService';
+import RecipeService from '../services/RecipeService';
+
+const recipeService = new RecipeService(process.env.BASE_URL);
+const authService = new AuthService(process.env.BASE_URL);
 
 const rootReducer = combineReducers({
   account: accountReducer,
@@ -10,4 +15,10 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: { recipeService, authService },
+      },
+    }),
 });
