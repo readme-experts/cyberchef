f140593a19bf395d0f1b2f3902ce6aadad31ef96
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-05-31 18:39:26 +0300

    feat: AuthService to login and register

---
+++ src/client/src/services/AuthService.js
@@ -0,0 +1,29 @@
+import BaseService from './BaseService';
+
+export default class AuthService extends BaseService {
+  async register(userData) {
+    try {
+      return await this.request('/register', 'POST', userData);
+    } catch (error) {
+      console.error('Error registering user:', error);
+      throw error;
+    }
+  }
+
+  async login(credentials) {
+    try {
+      const response = await this.request('/login', 'POST', credentials);
+      if (response.token) {
+        this.setToken(response.token);
+      }
+      return response;
+    } catch (error) {
+      console.error('Error logging in:', error);
+      throw error;
+    }
+  }
+
+  async logout() {
+    this.setToken(null);
+  }
+}
