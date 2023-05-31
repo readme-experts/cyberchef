f931e5694fdc8a02e3dd4a867530e0d490d074a7
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-05-31 20:08:20 +0300

    feat: rewrote login to use wrapper

--- src/client/src/app/actions/account/loginUser.js
+++ src/client/src/app/actions/account/loginUser.js
@@ -2,19 +2,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const loginUser = createAsyncThunk(
   'account/login',
-  async (credentials, thunkAPI) => {
-    try {
-      const response = await thunkAPI.extra.authService.login(credentials);
-      if (response.error) {
-        return thunkAPI.rejectWithValue(response.error);
-      }
-      return response;
-    } catch (error) {
-      if (error.response && error.response.data.message) {
-        return thunkAPI.rejectWithValue(error.response.data.message);
-      } else {
-        return thunkAPI.rejectWithValue(error.message);
-      }
-    }
+  async ({ credentials }, thunkAPI) => {
+    const thunk = thunkAPI.extra.thunkErrorWrapper(
+      thunkAPI.extra.authService.login,
+      thunkAPI.rejectWithValue,
+      thunkAPI.extra.authService
+    );
+    await thunk(credentials);
   }
 );
