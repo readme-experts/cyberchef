0b7e75c945f3afedf4817f0febaedd99545bc01d
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-05-31 20:08:12 +0300

    feat: rewrote register to use wrapper

--- src/client/src/app/actions/account/registerUser.js
+++ src/client/src/app/actions/account/registerUser.js
@@ -2,19 +2,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const registerUser = createAsyncThunk(
   'account/register',
-  async (credentials, thunkAPI) => {
-    try {
-      const response = await thunkAPI.extra.authService.register(credentials);
-      if (response.error) {
-        return thunkAPI.rejectWithValue(response.error);
-      }
-      return response;
-    } catch (error) {
-      if (error.response && error.response.data.message) {
-        return thunkAPI.rejectWithValue(error.response.data.message);
-      } else {
-        return thunkAPI.rejectWithValue(error.message);
-      }
-    }
+  async ({ credentials }, thunkAPI) => {
+    const thunk = thunkAPI.extra.thunkErrorWrapper(
+      thunkAPI.extra.authService.register,
+      thunkAPI.rejectWithValue,
+      thunkAPI.extra.authService
+    );
+    await thunk(credentials);
   },
 );
