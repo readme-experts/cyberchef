293109799931807f2bc18559212d1e8cb1fa1765
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-06 02:02:39 +0300

    refactor: add ts types to store

--- src/client/src/app/store.ts
+++ src/client/src/app/store.ts
@@ -1,9 +1,10 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { Action, combineReducers, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import accountReducer from './slices/accountSlice';
 import recipesReducer from './slices/recipesSlice';
 import AuthService from '../services/AuthService';
 import RecipeService from '../services/RecipeService';
 import { thunkErrorWrapper } from './utils/thunkErrorWrapper';
+import { useDispatch } from 'react-redux';
 
 const recipeService = new RecipeService(process.env.BASE_URL);
 const authService = new AuthService(process.env.BASE_URL);
@@ -25,3 +26,13 @@ const store = configureStore({
 });
 
 export { store, recipeService, authService };
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+export default store;
