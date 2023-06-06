d0bb87d1655b9b5567a805e50629fa79a1857a6d
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-06 03:25:11 +0300

    feat: typed accountSlice

--- src/client/src/app/slices/accountSlice.ts
+++ src/client/src/app/slices/accountSlice.ts
@@ -1,31 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { Action, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { loginUser } from '../actions/account/loginUser';
 import { registerUser } from '../actions/account/registerUser';
 import { loadFavoriteRecipes } from '../actions/account/loadFavoriteRecipes';
 import { addFavoriteRecipe } from '../actions/account/addFavoriteRecipe';
 import { deleteFavoriteRecipe } from '../actions/account/deleteFavoriteRecipe';
-
-const initialState = {
-  user: JSON.parse(localStorage.getItem('cyberChefUser')),
-  token: localStorage.getItem('cyberChefToken'),
-  userRecipes: [],
-  loading: false,
-  error: null,
-};
+import { AuthState } from './types/Auth/AuthState';
+import { AuthFulfilled } from './types/Auth/AuthFulfilled';
 
 const storageNames = {
   user: 'cyberChefUser',
   token: 'cyberChefToken',
 };
 
-const isRejectedAction = action => action.type.endsWith('rejected');
-const isPendingAction = action => action.type.endsWith('pending');
+const initialState: AuthState = {
+  user: JSON.parse(localStorage.getItem(storageNames.user) ?? ''),
+  token: localStorage.getItem(storageNames.token),
+  userRecipes: [],
+  loading: false,
+  error: null,
+};
+
+const isRejectedAction = (action: Action) => action.type.endsWith('rejected');
+const isPendingAction = (action: Action) => action.type.endsWith('pending');
+
+const saveUser = (state: AuthState, action: PayloadAction<AuthFulfilled>) => {
+  state.loading = false;
+  state.user = action.payload.user;
+  state.token = action.payload.accessToken;
+  localStorage.setItem(storageNames.user, JSON.stringify(state.user));
+  localStorage.setItem(storageNames.token, state.token as string);
+};
 
 const accountSlice = createSlice({
   name: 'account',
   initialState,
   reducers: {
-    logout(state) {
+    logout(state: AuthState) {
       state.user = null;
       state.token = null;
       localStorage.removeItem(storageNames.user);
@@ -34,26 +44,18 @@ const accountSlice = createSlice({
   },
   extraReducers: builder => {
     builder.addCase(loginUser.fulfilled, (state, action) => {
-      state.loading = false;
-      state.user = action.payload.user;
-      state.token = action.payload.accessToken;
-      localStorage.setItem(storageNames.user, JSON.stringify(state.user));
-      localStorage.setItem(storageNames.token, state.token);
+      saveUser(state, action);
     });
-    builder.addCase(registerUser.fulfilled, (state, action) => {
-      state.loading = false;
-      state.user = action.payload.user;
-      state.token = action.payload.accessToken;
-      localStorage.setItem(storageNames.user, JSON.stringify(state.user));
-      localStorage.setItem(storageNames.token, state.token);
+    builder.addCase(registerUser.fulfilled, (state: AuthState, action) => {
+      saveUser(state, action);
     });
-    builder.addCase(loadFavoriteRecipes.fulfilled, (state, action) => {
+    builder.addCase(loadFavoriteRecipes.fulfilled, (state: AuthState, action) => {
       state.userRecipes = action.payload;
     });
-    builder.addCase(addFavoriteRecipe.fulfilled, (state, action) => {
+    builder.addCase(addFavoriteRecipe.fulfilled, (state: AuthState, action) => {
       state.userRecipes.push(action.payload);
     });
-    builder.addCase(deleteFavoriteRecipe.fulfilled, (state, action) => {
+    builder.addCase(deleteFavoriteRecipe.fulfilled, (state: AuthState, action) => {
       state.userRecipes = state.userRecipes.filter(item => item.id !== action.payload.id);
     });
     builder.addMatcher(isRejectedAction, (state, action) => {
