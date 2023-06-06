4394535070a989e0073c90b65769e2f4bdd3fabd
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-06 03:23:45 +0300

    feat: typed thunkErrorWrapper

--- src/client/src/app/utils/thunkErrorWrapper.ts
+++ src/client/src/app/utils/thunkErrorWrapper.ts
@@ -1,11 +1,11 @@
-export const thunkErrorWrapper = (method, rejectWithValue, context = null) => async (...args) => {
+export const thunkErrorWrapper = (method: any, rejectWithValue: any, context = null) => async (...args : any) => {
   try {
     const response = await method.apply(context, args);
     if (response.error) {
       return rejectWithValue(response.error);
     }
     return response;
-  } catch (error) {
+  } catch (error: any) {
     return rejectWithValue(error.response.data.message ?? error.message);
   }
 };
