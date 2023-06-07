f8b1a42b74bf3e2c8ae36a08ee70e7f55d6dcd6a
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-07 18:42:01 +0300

    refactor: changed error names to avoid conflicts

--- src/client/src/app/slices/types/Auth/AuthState.ts
+++ src/client/src/app/slices/types/Auth/AuthState.ts
@@ -1,11 +1,11 @@
 import { User } from '../../../../services/models/User';
 import { RecipeModel } from '../../../../services/models/RecipeModel';
 
-export type AuthError =  Error | undefined | null;
+export type AuthStoreError =  Error | undefined | null;
 export interface AuthState {
   user: User | null,
   token: string | null,
   userRecipes: RecipeModel[],
   loading: boolean,
-  error: AuthError
+  error: AuthStoreError
 }
--- src/client/src/app/slices/types/Recipe/RecipeState.ts
+++ src/client/src/app/slices/types/Recipe/RecipeState.ts
@@ -1,8 +1,8 @@
 import { RecipeModel } from '../../../../services/models/RecipeModel';
 
-export type RecipeError = string | Error | null | undefined;
+export type RecipeStoreError =  Error | null | undefined;
 export interface RecipeState {
   recipes: RecipeModel[],
   loading: boolean,
-  error: RecipeError
+  error: RecipeStoreError
 }
