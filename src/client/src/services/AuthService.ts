25da082f388eded1c9b647ea4bfedf56958f72d0
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-07 17:56:34 +0300

    feat: typed AuthService.ts

--- src/client/src/services/AuthService.ts
+++ src/client/src/services/AuthService.ts
@@ -1,32 +1,30 @@
 import BaseService from './BaseService';
 import AuthError from './errors/AuthError';
+import { UserDTO } from './DTO/UserDTO';
+import { AuthFulfilled } from '../app/slices/types/Auth/AuthFulfilled';
 
 export default class AuthService extends BaseService {
-  constructor(baseURL) {
+  constructor(baseURL: string) {
     super(baseURL);
   }
 
-  async register(userData) {
+  async register(userData: UserDTO): Promise<AuthFulfilled> {
     try {
       return await this.request('/register', 'POST', userData);
     } catch (error) {
-      throw new AuthError('Error registering: ' + error.message);
+      throw new AuthError('Error registering: ' + (error as Error).message);
     }
   }
 
-  async login(credentials) {
+  async login(username: string, password: string): Promise<AuthFulfilled> {
     try {
-      const response = await this.request('/login', 'POST', credentials);
+      const response = await this.request('/login', 'POST', { username, password });
       if (response.token) {
         this.setToken(response.token);
       }
       return response;
     } catch (error) {
-      throw new AuthError('Error logging in: ' + error.message);
+      throw new AuthError('Error logging in: ' + (error as Error).message);
     }
-  }
-
-  async logout() {
-    this.setToken(null);
   }
 }
