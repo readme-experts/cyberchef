b537da273ea58f48b65207b3f204ee0bab068f30
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-07 18:25:38 +0300

    feat: typed class using enums

--- src/client/src/features/ValidationError.ts
+++ src/client/src/features/ValidationError.ts
@@ -1,24 +1,39 @@
+export const enum ValidationErrors {
+  Email = 'email',
+  Password = 'password',
+  Confirm = 'confirm',
+}
+
+export const enum ValidationDescriptions {
+  Email = 'Invalid email',
+  PasswordShort = 'Password is shorter than 8 symbols',
+  PasswordEmpty = 'Password is required',
+  Confirm = 'Password does not match',
+}
+
 export default class ValidationError {
-  constructor(type, description) {
+  type: string;
+  description: string;
+  constructor(type: ValidationErrors, description: ValidationDescriptions) {
     this.type = type;
     this.description = description;
   }
   static defaultPasswordLength = 8;
-  static validate(data) {
+  static validate(data: { email: string; password: string; confirm?: string; }) {
     const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
     const validationErrors = [];
     if (!emailRegex.test(data.email)) validationErrors.push(new ValidationError(
-      'email', 'Invalid email',
+      ValidationErrors.Email, ValidationDescriptions.Email,
     ));
     if (data.password.length < ValidationError.defaultPasswordLength)
       validationErrors.push(new ValidationError(
-      'password', 'Password is shorter than 8 symbols',
-    ));
+        ValidationErrors.Password, ValidationDescriptions.PasswordShort,
+      ));
     if (!data.password || !data.password.length) validationErrors.push(new ValidationError(
-      'password', 'Password is required',
+      ValidationErrors.Password, ValidationDescriptions.PasswordEmpty,
     ));
     if (data.confirm && data.confirm !== data.password) validationErrors.push(new ValidationError(
-      'confirm', 'Password does not match',
+      ValidationErrors.Confirm, ValidationDescriptions.Confirm,
     ));
     return validationErrors;
   }
