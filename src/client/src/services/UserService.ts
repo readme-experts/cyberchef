dd212893978272ae851cdf5271884cb65dfa8635
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-07 17:50:00 +0300

    feat: typed UserService

--- src/client/src/services/UserService.ts
+++ src/client/src/services/UserService.ts
@@ -1,39 +1,40 @@
 import BaseService from './BaseService';
 import UserError from './errors/UserError';
+import { Recipe } from './models/Recipe';
 
 export default class UserService extends BaseService {
-  constructor(baseURL) {
+  constructor(baseURL: string) {
     super(baseURL);
   }
 
-  async getUserRecipes() {
+  async getUserRecipes(): Promise<Recipe[]> {
     try {
       return await this.request(`/user/authored`, 'GET');
     } catch (error) {
-      throw new UserError('Error getting authored recipes: ' + error.message);
+      throw new UserError('Error getting authored recipes: ' + (error as Error).message);
     }
   }
 
-  async getFavoriteRecipes() {
+  async getFavoriteRecipes(): Promise<Recipe[]>  {
     try {
       return await this.request(`/user`, 'GET');
     } catch (error) {
-      throw new UserError('Error getting favorite recipes: ' + error.message);
+      throw new UserError('Error getting favorite recipes: ' + (error as Error).message);
     }
   }
 
-  async addFavoriteRecipe(userId, recipeId) {
+  async addFavoriteRecipe(userId: number, recipeId: number) {
     try {
       return await this.request(`/user`, 'POST', { userId, recipeId });
     } catch (error) {
-      throw new UserError('Error adding favorite recipe: ' + error.message);
+      throw new UserError('Error adding favorite recipe: ' + (error as Error).message);
     }
   }
-  async deleteFavoriteRecipe(recipeId) {
+  async deleteFavoriteRecipe(recipeId: number) {
     try {
       return await this.request(`/user`, 'DELETE', recipeId);
     } catch (error) {
-      throw new UserError('Error deleting favorite recipe: ' + error.message);
+      throw new UserError('Error deleting favorite recipe: ' + (error as Error).message);
     }
   }
 
