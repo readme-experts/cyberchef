47f2c627d797bb259a347d68d5d7e17fb316b9e0
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-07 18:54:09 +0300

    feat: typed SearchRecipes page

--- src/client/src/pages/SearchRecipes.tsx
+++ src/client/src/pages/SearchRecipes.tsx
@@ -1,23 +1,26 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import Search from '../components/Search';
 import { loadRecipes } from '../app/actions/recipes/loadRecipes';
 import { addFavoriteRecipe } from '../app/actions/account/addFavoriteRecipe';
 import Recipe from '../components/Recipe';
 import Loader from '../components/Loader';
 import ErrorMessage from '../components/ErrorMessage';
+import { useAppDispatch, useAppSelector } from '../app/store';
+import { RecipeModel } from '../services/models/RecipeModel';
 
 function SearchRecipes() {
-  const [recipes, setRecipes] = useState([]);
-  const storeRecipes = useSelector(state => state.recipes.recipes);
-  const dispatch = useDispatch();
-  const { error, loading } = useSelector(state => state.recipes.recipes);
-  const searchCallback = useCallback((event, { queryString }) => {
-    event.preventDefault();
-    dispatch(loadRecipes(queryString));
-  }, []);
+  const [recipes, setRecipes] = useState<RecipeModel[]>([]);
+  const { error, loading } = useAppSelector(state => state.recipes);
+  const storeRecipes = useAppSelector(state => state.recipes.recipes);
+  const dispatch = useAppDispatch();
+  const searchCallback = useCallback(
+    (event: React.FormEvent<HTMLFormElement>,
+      { queryString }: { queryString: string }) => {
+      event.preventDefault();
+      dispatch(loadRecipes(queryString));
+    }, []);
   const clickCallback = useCallback(
-    recipe => dispatch(addFavoriteRecipe(recipe)),
+    (recipe: RecipeModel) => dispatch(addFavoriteRecipe(recipe)),
     [],
   );
 
@@ -37,7 +40,7 @@ function SearchRecipes() {
           <div className='content__recipes'>
             {recipes.map((recipe, index) => <Recipe recipe={recipe}
               clickHandler={clickCallback}
-              key={index} />)}
+              key={index}/>)}
           </div> :
           <h3>No recipes</h3>}
       </div>
