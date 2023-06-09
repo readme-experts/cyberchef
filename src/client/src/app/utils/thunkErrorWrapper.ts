bd2282711310251fca2ea5f20981a99ae896e08e
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-06-09 19:41:25 +0300

    refactor: typed thunkWrapper using generics

--- src/client/src/app/utils/thunkErrorWrapper.ts
+++ src/client/src/app/utils/thunkErrorWrapper.ts
@@ -1,11 +1,17 @@
-export const thunkErrorWrapper = (method: any, rejectWithValue: any, context = null) => async (...args : any) => {
-  try {
-    const response = await method.apply(context, args);
-    if (response.error) {
-      return rejectWithValue(response.error);
+import BaseService from '../../services/BaseService';
+
+export function thunkErrorWrapper<ReturnType, Args>(method: (...args : Args[]) => ReturnType,
+  rejectWithValue: (error: string) => void,
+  context: BaseService | null = null) {
+  return async (...args : Args[]) => {
+    try {
+      const response = await method.apply(context, args);
+      if ((response as any).error) {
+        return rejectWithValue((response as any).error);
+      }
+      return response;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data.message ?? error.message);
     }
-    return response;
-  } catch (error: any) {
-    return rejectWithValue(error.response.data.message ?? error.message);
-  }
-};
+  };
+}
