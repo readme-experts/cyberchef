806395a88b62ca59d39fda2d45421a67bdb5c430
thank1ess <vladonyaroshchuk@gmail.com>
2023-06-13 16:24:59 +0300

    Json beautifier done

--- src/parser/index.js
+++ src/parser/index.js
@@ -2,10 +2,9 @@
 
 const infoHandler = require('./handlers/infoHandler');
 const saver = require('./handlers/saver');
-const beautify = require ('./handlers/beautifier');
 
 (async () => {
   const links = await infoHandler.getLinks();
   const recipes = await infoHandler.getRecipes(links);
-  await saver.saveData(beautify.fix(recipes));
+  await saver.saveData(recipes);
 })();
